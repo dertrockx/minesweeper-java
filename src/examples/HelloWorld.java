78b407eced38f9aaa0d1df95a41fddacd63989fb
Ian Salazar <iansalazar67@gmail.com>
2020-11-06 22:29:12 +0800

    Added a new button that handles exiting the program

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -5,32 +5,63 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HelloWorld {
     private Scene scene;
-    private StackPane root;
+    private VBox root;
 
     public HelloWorld(){
-        this.root = new StackPane();
+        this.root = new VBox();
         this.scene = new Scene(root, 500, 300);
     }
 
     public void setStageComponents(Stage stage){
-        Button btn = new Button("Click me!");
+//        Button btn = new Button("Click me!");
+//
+//        btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//                System.out.println("Daddy clicked me while inside hello world!");
+//            }
+//        });
+//
+////        add components to root stack
+//        this.root.getChildren().add( btn );
 
-        btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        Button clickMe = new Button("Click me daddy");
+        Button exit = new Button("Close the door when you leave");
+
+        clickMe.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                System.out.println("Daddy clicked me while inside hello world!");
+                handleButtonClick("clickMe");
             }
         });
 
-//        add components to root stack
-        this.root.getChildren().add( btn );
+        exit.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                handleButtonClick("exit");
+            }
+        });
+
+
+        this.root.getChildren().add( clickMe );
+        this.root.getChildren().add( exit );
 
         stage.setTitle("Hello world with extra shemes");
         stage.setScene(this.scene);
         stage.show();
+    }
+
+    private void handleButtonClick(String btnName) {
+        if(btnName.contentEquals("clickMe")){
+            System.out.println("Daddy clicked me with a twist");
+        } else {
+            System.out.println("End of program! Byeeee");
+            System.exit(0);
+        }
     }
 }
