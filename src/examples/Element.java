091536fe394acabd7bbd7ad12c1298c1830c6461
Ian Salazar <iansalazar67@gmail.com>
2020-11-07 15:07:31 +0800

    Finished the game

--- src/examples/Element.java
+++ src/examples/Element.java
@@ -6,18 +6,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 
-import javafx.event.EventHandler;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import examples.GameStage;
-
 public class Element {
     private String type;
     protected Image img;
     protected ImageView imgView;
     protected GameStage gameStage;
     protected int row, col;
+
 
     public final static Image FLAG_IMAGE = new Image("images/flag.png",GameStage.CELL_WIDTH,GameStage.CELL_WIDTH,false,false);
     public final static Image BOMB_IMAGE = new Image("images/bomb.gif",GameStage.CELL_WIDTH,GameStage.CELL_WIDTH,false,false);
@@ -35,20 +30,61 @@ public class Element {
 
         // load the images depending on the type
         switch(type){
-            case FLAG_TYPE:
-                this.img = FLAG_IMAGE;
-                break;
-            case BOMB_TYPE:
-                this.img = BOMB_IMAGE;
-                break;
-            case LAND_TYPE:
-                this.img = LAND_IMAGE;
-                break;
+            case FLAG_TYPE: this.img = Element.FLAG_IMAGE; break;
+            case BOMB_TYPE: this.img = Element.BOMB_IMAGE; break;
+            case LAND_TYPE: this.img = Element.LAND_IMAGE; break;
         }
         // call the functions that sets the image and handles the events
         this.setImageView();
-//        this.handleEvents;
+        this.setEventHanlders();
     }
+
+    private void setEventHanlders(){
+        Element el = this;
+        this.imgView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                switch(el.getType()){
+                    case Element.FLAG_TYPE:
+                        System.out.println("Flag clicked!");
+                        gameStage.setFlagClicked(true);
+                        break;
+                    case Element.LAND_TYPE:
+                        System.out.println("Land clicked!");
+                        if(!gameStage.isFlagClicked()){
+                            if( !gameStage.isBomb(el)){
+                                clearImage(el);
+                            } else {
+                                changeImage(el, Element.BOMB_IMAGE);
+                                gameStage.flashGameOver();
+                            }
+                        } else {
+                            changeImage(el, Element.FLAG_IMAGE);
+                            el.setType(LAND_FLAG_TYPE);
+                            gameStage.setFlagClicked(false);
+                        }
+                        break;
+                    case Element.LAND_FLAG_TYPE:
+                        changeImage(el, Element.LAND_IMAGE);
+                        el.setType(LAND_TYPE);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void setType(String type) {
+        this.type = type;
+    }
+
+    private void clearImage(Element el){
+        this.imgView.setImage(null);
+    }
+
+    private void changeImage(Element el, Image image){
+        this.imgView.setImage(image);
+    }
+
 
     String getType(){
         return this.type;
@@ -62,6 +98,10 @@ public class Element {
         return this.col;
     }
 
+    void initRowCol(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
 
     protected ImageView getImageView(){
         return this.imgView;
@@ -69,6 +109,19 @@ public class Element {
 
     // implement the necessary setters
     private void setImageView(){
+        this.imgView = new ImageView();
+        this.imgView.setImage( this.img );
+        this.imgView.setLayoutX(0);
+        this.imgView.setLayoutY(0);
+        this.imgView.setPreserveRatio(true);
+
+        if( this.type.equals(Element.FLAG_TYPE)){
+            this.imgView.setFitWidth(GameStage.FLAG_WIDTH);
+            this.imgView.setFitHeight(GameStage.FLAG_HEIGHT);
+        } else {
+            this.imgView.setFitHeight(GameStage.CELL_HEIGHT);
+            this.imgView.setFitWidth(GameStage.CELL_WIDTH);
+        }
 
     }
     // implement the functions that initializes the image view of this element and the event handlers you need
--- src/examples/GameStage.java
+++ src/examples/GameStage.java
@@ -28,6 +28,11 @@ public class GameStage {
     private GraphicsContext gc;
 
     private int[][] gameBoard;
+    private GridPane map;
+    private boolean isFlagClicked = false;
+    private ArrayList<Element> landElements;
+
+    private Element flag;
 
     public final static int MAX_CELLS = 9;
     public final static int MAP_NUM_ROWS = 3;
@@ -49,8 +54,10 @@ public class GameStage {
         this.scene = new Scene(root, GameStage.WINDOW_WIDTH,GameStage.WINDOW_HEIGHT,Color.WHITE);
         this.canvas = new Canvas(GameStage.WINDOW_WIDTH,GameStage.WINDOW_HEIGHT);
         this.gc = canvas.getGraphicsContext2D();
-
+        this.flag = new Element(Element.FLAG_TYPE, this);
         this.gameBoard = new int[GameStage.MAP_NUM_ROWS][GameStage.MAP_NUM_COLS];
+        this.landElements = new ArrayList<>();
+        this.map = new GridPane();
     }
 
     //method to add the stage elements
@@ -60,9 +67,12 @@ public class GameStage {
         this.gc.drawImage( this.bg, 0, 70);
 
         this.initGameBoard();
+        this.drawMap();
 
         //set stage elements here
         this.root.getChildren().add(canvas);
+        this.root.getChildren().add(map);
+        this.root.getChildren().add(this.flag.getImageView());
         this.stage.setTitle("Modified Minesweeper Game");
         this.stage.setScene(this.scene);
         this.stage.show();
@@ -84,17 +94,82 @@ public class GameStage {
     }
 
 //    draw the game baord
-    private void drawBoard(){
+    private void drawMap(){
         for(int i = 0; i < GameStage.MAP_NUM_ROWS; i++){
-            for(int j = 0; i < GameStage.MAP_NUM_COLS; j++){
-
+            for(int j = 0; j < GameStage.MAP_NUM_COLS; j++){
+                Element el = new Element(Element.LAND_TYPE, this);
+                el.initRowCol(i, j);
+                this.landElements.add(el);
             }
         }
+
+        this.setGridPaneProps();
+        this.setGridPaneContent();
     }
-    // implement the necessary getters and setters
 
-    // add implementation of changing from GameStage to GameOverStage
+    private void setGridPaneProps(){
+//        set preferred size of map gridpane
+        try {
+            this.map.setPrefSize(GameStage.MAP_WIDTH, GameStage.MAP_HEIGHT);
+
+            this.map.setLayoutX( GameStage.WINDOW_WIDTH * 0.15 );
+            this.map.setLayoutY( GameStage.WINDOW_HEIGHT * 0.15 );
+//        vertical padding
+            this.map.setVgap(5);
+//        horizontal padding
+            this.map.setHgap(5);
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private void setGridPaneContent(){
+        int counter = 0;
+
+        for(int row = 0; row < GameStage.MAP_NUM_ROWS; row++){
+            for(int col = 0; col < GameStage.MAP_NUM_COLS; col++){
+                GridPane.setConstraints( landElements.get(counter).getImageView(), col, row );
+                counter++;
+            }
+        }
+
+        for(Element landEl: this.landElements){
+            this.map.getChildren().add( landEl.getImageView() );
+        }
+    }
+
+    public boolean isFlagClicked() {
+        return this.isFlagClicked;
+    }
+
+    public void setFlagClicked(boolean value) {
+        this.isFlagClicked = value;
+    }
+
+    public boolean isBomb(Element el){
+        int i = el.getRow(), j = el.getCol();
+
+        if( this.gameBoard[i][j] == 1 ){
+            System.out.println(">>>>>>>>>>>> BOMB!!!");
+            return true;
+        }
+        System.out.println(">>>>>>>>>>>>>> SAFE!!!");
+        return false;
+    }
 
 
+    public void flashGameOver() {
+        PauseTransition transition = new PauseTransition(Duration.seconds(1));
+        transition.play();
+
+        transition.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                GameOverStage gameOver = new GameOverStage();
+                stage.setScene(gameOver.getScene());
+            }
+        });
+    }
 }
 
