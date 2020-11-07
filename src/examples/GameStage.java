afdb5fea6b3f9a3e6508f25aed68bc72b4ed9ce7
Ian Salazar <iansalazar67@gmail.com>
2020-11-07 10:49:20 +0800

    ongoing work: add method to create gameBoard

--- src/examples/GameStage.java
+++ src/examples/GameStage.java
@@ -1,113 +1,100 @@
 package examples;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class GameStage {
     private Scene scene;
     private Stage stage;
+    /*Group layout/container is a component which applies no special
+    layout to its children. All child components (nodes) are positioned at 0,0*/
     private Group root;
     private Canvas canvas;
     private GraphicsContext gc;
-//    private ImageView poro;
-//    private ImageView biscuit;
-    private Image poro;
-    private Image biscuit;
-    private int poroXpos;
-//    private VBox root;
 
-    public GameStage(){
-//        this.root = new VBox();
-//        this.scene = new Scene(root, 500, 300);
+    private int[][] gameBoard;
+
+    public final static int MAX_CELLS = 9;
+    public final static int MAP_NUM_ROWS = 3;
+    public final static int MAP_NUM_COLS = 3;
+    public final static int MAP_WIDTH = 400;
+    public final static int MAP_HEIGHT = 400;
+    public final static int CELL_WIDTH = 115;
+    public final static int CELL_HEIGHT = 115;
+    public final static int FLAG_WIDTH = 70;
+    public final static int FLAG_HEIGHT = 70;
+    public final static boolean IS_GAME_DONE = false;
+    public final static int WINDOW_WIDTH = 500;
+    public final static int WINDOW_HEIGHT = 500;
+
+    public final Image bg = new Image("images/background.jpg",500,500,false,false);
+
+    public GameStage() {
         this.root = new Group();
-
-//        create a 400 x 400 application with black background
-        this.scene = new Scene(root, 400, 400, Color.BLACK);
-//        create a 400 x 400 canvas for "drawing" element
-        this.canvas = new Canvas(400, 400);
+        this.scene = new Scene(root, GameStage.WINDOW_WIDTH,GameStage.WINDOW_HEIGHT,Color.WHITE);
+        this.canvas = new Canvas(GameStage.WINDOW_WIDTH,GameStage.WINDOW_HEIGHT);
         this.gc = canvas.getGraphicsContext2D();
 
-//        to resize image upon loading
-        this.poro = new Image("images/poro.png", 120, 120, false, false);
-        this.biscuit = new Image("images/biscuit.png", 50, 50, false, false);
-//        this.poro = new ImageView("images/poro.png" );
-//        this.buiscuit = new ImageView("images/biscuit.png");
-        this.poroXpos = 150;
+        this.gameBoard = new int[GameStage.MAP_NUM_ROWS][GameStage.MAP_NUM_COLS];
     }
 
+    //method to add the stage elements
     public void setStage(Stage stage) {
         this.stage = stage;
+        //draw the background to the canvas at location x=0, y=70
+        this.gc.drawImage( this.bg, 0, 70);
 
-        this.addComponents();
-        this.handleKeyPressEvent();
-//        set stage elements
-        this.root.getChildren().add( canvas );
-        this.stage.setTitle("The first Game Application");
-        this.stage.setScene( this.scene );
+        this.initGameBoard();
+
+        //set stage elements here
+        this.root.getChildren().add(canvas);
+        this.stage.setTitle("Modified Minesweeper Game");
+        this.stage.setScene(this.scene);
         this.stage.show();
-
-
     }
 
-    private void addComponents(){
-        gc.clearRect(0,0,400,400);
-//        sets font style
-        Font theFont = Font.font("Ubuntu", FontWeight.BOLD, 30);
-        this.gc.setFill(Color.PINK);
-
-        this.gc.setFont( theFont );
-//        add a "hello world" to location x=60, y=50
-        this.gc.fillText("Hello World", 60, 50);
-        this.gc.setFill(Color.GREEN);
-
-        this.gc.fillText("First GUI Example", 60, 120);
-        //to load the image; original size
-//        Image poro = new Image("images/poro.png");
-//        gc.drawImage(poro, 150, 150);
-        this.gc.drawImage(poro, this.poroXpos, 150);
-        this.gc.drawImage(biscuit, 300, 200);
-    }
-
-    private void handleKeyPressEvent(){
-//        TODO: Add setOnKeyPress() event handler here
-        this.scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                if(keyEvent.getCode() == KeyCode.RIGHT){
-                    setDx(10);
-                    System.out.println("Move right!");
-                } else if(keyEvent.getCode() == KeyCode.LEFT){
-                    setDx(-10);
-                    System.out.println("Move left");
-                }
+    // place your implementation of the game here; the game logic and the layout
+//    initialize game board
+    private void initGameBoard(){
+        Random rand = new Random();
+        for(int i = 0;i < GameStage.MAP_NUM_ROWS; i++){
+            for(int j = 0; j < GameStage.MAP_NUM_COLS; j++){
+                this.gameBoard[i][j] = rand.nextInt(2);
             }
-        });
+        }
+//        prints the board
+        for(int i = 0; i < GameStage.MAP_NUM_ROWS; i++){
+            System.out.println(Arrays.toString( this.gameBoard[i] ));
+        }
     }
 
-    private void setDx(int dx){
-//        TODO: CHANGE x position of poro here and redraw canvas components
-        this.poroXpos += dx;
-//         since components are not reactive
-//        you have to manually reload the components
-        this.addComponents();
+//    draw the game baord
+    private void drawBoard(){
+        for(int i = 0; i < GameStage.MAP_NUM_ROWS; i++){
+            for(int j = 0; i < GameStage.MAP_NUM_COLS; j++){
+
+            }
+        }
     }
+    // implement the necessary getters and setters
 
-
+    // add implementation of changing from GameStage to GameOverStage
 
 
 }
+
